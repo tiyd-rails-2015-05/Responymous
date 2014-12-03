4ce518c97055fee0e7fbdbc79787b3b18e79c90d
David Elmer <davelmer@hotmail.com>
2014-12-03 10:45:50 -0500

    Reorganized file structure

---
+++ src/app/js/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('responymous', ['ngAnimate', 'ngCookies', 'ngTouch', 'ui.router', 'firebase'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('student', {
+        url: '/student',
+        templateUrl: 'app/partials/student.html'
+      })
+      .state('instructor', {
+        url: '/instructor',
+        templateUrl: 'app/partials/instructor.html'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ src/app/js/main.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('responymous')
+  .controller('MainCtrl', function() {
+
+  });
