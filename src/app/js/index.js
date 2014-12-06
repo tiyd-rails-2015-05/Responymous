89cfd0c51560abf885003455230151d7c30a8616
David Elmer <davelmer@hotmail.com>
2014-12-05 22:19:30 -0500

    Added firebase urtl config

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -3,20 +3,26 @@
 angular.module('responymous', ['ngAnimate', 'ngCookies', 'ngTouch', 'ui.router', 'firebase'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/partials/main.html',
-        controller: 'MainCtrl'
-      })
-      .state('student', {
-        url: '/student',
-        templateUrl: 'app/partials/student.html'
-      })
-      .state('instructor', {
-        url: '/instructor',
-        templateUrl: 'app/partials/instructor.html'
-      });
-
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/partials/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('student', {
+      url: '/student',
+      templateUrl: 'app/partials/student.html'
+    })
+    .state('instructor', {
+      url: '/instructor',
+      templateUrl: 'app/partials/instructor.html',
+      controller: 'ResponseCtrl'
+    });
+    
     $urlRouterProvider.otherwise('/');
+  })
+  .constant('CONFIG', {
+    Firebase: {
+      baseUrl: 'https://responymousdevdb.firebaseio.com'
+    }
   })
 ;
