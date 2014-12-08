d4d7cd9832d8ab7d6207863b00e0e4cd0872581e
David Elmer <davelmer@hotmail.com>
2014-12-08 12:35:58 -0500

    Started saving data to multiple tables

--- src/app/js/main.controller.js
+++ src/app/js/main.controller.js
@@ -53,8 +53,33 @@ angular.module('responymous')
         access_token: authdUser.github.accessToken,
         email: authdUser.github.email,
         name: authdUser.github.displayName,
+        last_vote: 5,
         current_class: "Q42014FEEORL",
-        role: "student"
+        student: true
+      });
+
+      var userClass = $firebase(Firebase
+        .child('userClasses')
+        .push({
+          class_id: "Q42014FEEORL",
+          user_id: authdUser.github.id
+      }).$asObject();
+
+      // *** Generates double entries!! *** WHY???
+      Firebase.child("userClasses").push({
+        class_id: "Q42014FEEORL",
+        user_id: authdUser.github.id
+      });
+
+      // *** Fail - neither students or size child nodes update ***
+      Firebase.child("classes/size").transaction(function(snap_size){
+        var arrayNum = snap_size;
+        Firebase.child("classes/students").set(
+          { arrayNum : authdUser.github.id }
+        );
+        return snap_size + 1;
+      }, function (err) {
+        // code to handle read error
       });
 
       user.$save();
