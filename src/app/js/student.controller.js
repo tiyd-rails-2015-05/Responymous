647052fdb50504213c5be4b25a919b3a6138579e
David Elmer <davelmer@hotmail.com>
2014-12-09 13:57:38 -0500

    Mod student ctrl to angfire and renamed instructor ctrl

--- src/app/js/student.controller.js
+++ src/app/js/student.controller.js
@@ -1,11 +1,8 @@
 'use strict';
 
 angular.module('responymous')
- .controller('StudentCtrl', function(CONFIG, $timeout) {
+ .controller('StudentCtrl', function(Firebase, $timeout, $firebase) {
 
-
-
-  var ref = new Firebase(CONFIG.Firebase.baseUrl);
   var self = this;
 
   this.isDisabled = false;
@@ -21,47 +18,33 @@ angular.module('responymous')
     var userID = "8822941";
     var classID = "Q42014FEEORL";
 
-    //Record vote in Firebase
-    ref.child("votes").push({
+    var vote = $firebase(Firebase
+      .child('votes')
+    ).$asArray();
+
+    vote.$add({
       class_id: classID,
-      timestamp: Firebase.ServerValue.TIMESTAMP,
       date: currDate,
       score: selection,
       student_id: userID
     });
 
-    var user_LastVote = ref.child("users/"+userID+"/last_vote");
-    var class_VoteTotals = ref.child("classes/"+classID+"/vote_totals");
+    var user = $firebase(Firebase
+      .child('users').child(userID)
+    ).$asObject();
 
-    user_LastVote.once("value", function(snap_user){
-      class_VoteTotals.once("value", function(snap_class){
-
-        var class_LastVote = class_VoteTotals.child(snap_user.val());
-
-        class_LastVote.transaction(function(snap_lastVote){
-
-          //Decrements class tally for user last vote
-          return snap_lastVote - 1;
-        });
-
-        var class_CurrVote = class_VoteTotals.child(selection);
-        class_CurrVote.transaction(function(snap_currVote){
-
-          //Increments class tally for user selection
-          return snap_currVote + 1;
-        });
-
-        //Updates user last vote
-        user_LastVote.set(selection);
-      });
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
+    user.$loaded().then(function(){
+      user.last_vote = selection;
+      user.$save();
     });
+
+    var user2 = $firebase(Firebase
+      .child('users')
+    ).$asObject();
 
     $timeout(function(){
       self.isDisabled=false;
     }, 3000);
-
 
   };
 })
